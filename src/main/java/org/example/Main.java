b09283a9c918c2a9f54d9b7e2f2fbea597b23b93
MarioHS <mario.hernandezgithub@gmail.com>
2023-08-29 19:30:47 -0600

    Mensaje de prueba x2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,6 +13,8 @@ public class Main {
         rectángulo.setLado1(13);
         rectángulo.setLado2(6);
 
+        //Mensaje de prueba
+
         JOptionPane.showMessageDialog(null,"El area del circulo es: " + circulo.calcularArea() +
                 "\n y del rectangulo es: " + rectángulo.calcularArea());
         }
